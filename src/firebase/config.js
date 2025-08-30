875440dc7e2d245595a4852b83768e9946bf87fe
uaonline07-web <uaonline07@gmail.com>
2025-08-31 01:47:22 +0500

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,3 +5,13 @@ export const cloudinaryConfig = {
   apiSecret: "M7EOvO5clu5j2EA5iinvqMn44tw",
   uploadPreset: "ml_default" // Cloudinary default preset
 };
+const firebaseConfig = {
+  apiKey: "AIzaSyAo6wWzDRyGcg9PYZW-od-Z2K7WWwjAIiU",
+  authDomain: "olx-demo-xyz.firebaseapp.com",
+  projectId: "olx-demo-xyz",
+  storageBucket: "olx-demo-xyz.firebasestorage.app",
+  messagingSenderId: "659332501997",
+  appId: "1:659332501997:web:58aa1c0c69704cc18d57e4",
+  measurementId: "G-NJSQGK8QYG"
+};
+export default firebaseConfig;
